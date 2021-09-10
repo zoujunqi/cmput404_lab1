cd2fe12879e1a30e118fa66ba9506f000c05aaea
zoujunqi <55213549+zoujunqi@users.noreply.github.com>
2021-09-10 23:53:55 +0800

    Update request.py
    update to print content of current url

--- request.py
+++ request.py
@@ -1,3 +1,6 @@
 import requests
 response = requests.get("http://www.google.com/")
 print(response)
+my_url = 'https://github.com/zoujunqi/cmput404_lab1/bloab/main/request.py'
+content = requests.get(my_url)
+print(content.text)
